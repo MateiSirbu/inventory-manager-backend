a8913afef2d63202134c052442bf3d3fdac23749
Matei Sîrbu <matei.sirbu@student.unitbv.ro>
2020-08-06 15:20:31 +0300

    Added unit tests for a-json.data, a-json.route test is WIP

--- spec/demo.spec.ts
+++ spec/demo.spec.ts
@@ -1,4 +1,4 @@
-describe("test suite", () => {
-    it("test 1", () => expect(1).toBe(1, "this test should not fail"));
-    it("test 2", () => expect(1).toBe(2, "this test failed miserably"));
-});
+// describe("test suite", () => {
+//     it("test 1", () => expect(1).toBe(1, "this test should not fail"));
+//     it("test 2", () => expect(1).toBe(2, "this test failed miserably"));
+// });
