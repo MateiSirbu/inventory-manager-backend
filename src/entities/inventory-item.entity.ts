1cec4324f554a63a032596212ca1ad5f9b30b0c6
Matei Sîrbu <matei.sirbu@student.unitbv.ro>
2020-09-22 23:31:04 +0300

    Added new fields in inventory-item.entity

--- src/entities/inventory-item.entity.ts
+++ src/entities/inventory-item.entity.ts
@@ -1,48 +1,50 @@
-import {
-    Entity,
-    MongoEntity,
-    SerializedPrimaryKey,
-    PrimaryKey,
-    Property,
-  } from "mikro-orm";
-  import { ObjectId } from "mongodb";
-  import { IInventoryItem } from "inventory-interfaces/inventoryitem"
-  
-  @Entity()
-  export class InventoryItem implements MongoEntity<InventoryItem>, IInventoryItem {
-    @PrimaryKey()
-    _id!: ObjectId;
-  
-    @SerializedPrimaryKey()
-    id!: string;
-  
-    @Property()
-    name: string;
-  
-    @Property()
-    description: string;
-  
-    @Property()
-    user: string;
-  
-    @Property()
-    location: string;
-  
-    @Property()
-    inventoryNumber: number;
-  
-    @Property()
-    createdAt: Date;
-  
-    @Property()
-    modifiedAt: Date;
-  
-    @Property()
-    active: boolean;
-  
-    public constructor(init?: Partial<InventoryItem>) {
-      this.createdAt = new Date();
-      Object.assign(this, init);
-    }
+import { Entity, MongoEntity, SerializedPrimaryKey, PrimaryKey, Property } from "mikro-orm";
+import { ObjectId } from "mongodb";
+import { IInventoryItem } from "inventory-interfaces/inventoryitem"
+
+@Entity()
+export class InventoryItem implements MongoEntity<InventoryItem>, IInventoryItem {
+  @PrimaryKey()
+  _id!: ObjectId;
+
+  @SerializedPrimaryKey()
+  id!: string;
+
+  @Property()
+  name: string;
+
+  @Property()
+  description: string;
+
+  @Property()
+  user: string;
+
+  @Property()
+  location: string;
+
+  @Property()
+  inventoryNumber: number;
+
+  @Property()
+  createdAt: Date;
+
+  @Property()
+  addedAt: Date;
+
+  @Property()
+  addedBy: string;
+
+  @Property()
+  modifiedAt: Date;
+
+  @Property()
+  modifiedBy: string;
+
+  @Property()
+  active: boolean;
+
+  public constructor(init?: Partial<InventoryItem>) {
+    this.createdAt = new Date();
+    Object.assign(this, init);
   }
-  
+}
